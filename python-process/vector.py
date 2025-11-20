18e1103d4afd76b98c2bc97b54c73def6f01460c
uiv78928 <uiv78928@vitesco.com>
2025-11-20 10:51:51 +0200

    added pyhton code

---
+++ python-process/vector.py
@@ -0,0 +1,10 @@
+# Definim un vector cu valori dummy
+vector = [10, 20, 30, 40, 50]
+
+# Afișăm vectorul complet
+print("Vectorul complet:", vector)
+
+# Afișăm elementele vectorului unul câte unul
+print("Elementele vectorului:")
+for element in vector:
+    print(element)
